a6756fcccc3671b4f7a735127e9d7e61ed5213cc
unknown <ibmjavanh15@iiht.tech>
2021-03-30 16:27:36 +0530

    Another commit

--- project-service/src/main/java/com/ibm/project/controller/ProjectController.java
+++ project-service/src/main/java/com/ibm/project/controller/ProjectController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ibm.project.entity.Project;
 import com.ibm.project.service.ProjectService;
 
 @RestController
@@ -24,6 +25,12 @@ public class ProjectController {
 	@Autowired
 	ProjectService projectService;
 
+	/**
+	 * method to create project
+	 * 
+	 * @param project
+	 * @param bindingResult returns project id
+	 */
 	@PostMapping("/project")
 	@ResponseStatus(code = HttpStatus.CREATED)
 	String createProject(@RequestBody @Valid Project project, BindingResult bindingResult) {
@@ -33,21 +40,36 @@ public class ProjectController {
 	}
 
 	private void validateProject(BindingResult bindingResult) {
-		if(bindingResult.hasErrors()) {
+		if (bindingResult.hasErrors()) {
 			throw new IllegalArgumentException("Something went wrong, please retry.");
 		}
 	}
 
+	/**
+	 * method to search all projects returns list of projects
+	 */
 	@GetMapping("/project")
 	List<Project> getProjects() {
 		return projectService.getProjects();
 	}
 
+	/**
+	 * method to search for project by id
+	 * 
+	 * @param projectId returns zero or matching project
+	 */
 	@GetMapping("/project/{id}")
 	Optional<Project> getProject(@PathVariable("id") String projectId) {
 		return projectService.getProject(projectId);
 	}
 
+	/**
+	 * method to update project details
+	 * 
+	 * @param project
+	 * @param bindingResult
+	 * @param projectId
+	 */
 	@PutMapping("/project/{id}")
 	void updateProject(@RequestBody @Valid Project project, BindingResult bindingResult,
 			@PathVariable("id") String projectId) {
