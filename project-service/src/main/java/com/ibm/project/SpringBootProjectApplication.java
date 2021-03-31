4a47e44e5a958f178ffe65ec3516eff763325078
unknown <ibmjavanh15@iiht.tech>
2021-03-31 10:21:38 +0530

    Added Transactional Support

--- project-service/src/main/java/com/ibm/project/SpringBootProjectApplication.java
+++ project-service/src/main/java/com/ibm/project/SpringBootProjectApplication.java
@@ -2,12 +2,19 @@ package com.ibm.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.MongoTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class SpringBootProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootProjectApplication.class, args);
 	}
 
+	PlatformTransactionManager transactionManger(){
+		return new MongoTransactionManager();
+	}
 }
