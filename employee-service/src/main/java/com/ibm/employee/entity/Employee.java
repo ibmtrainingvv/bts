165fd1e676c2a4286280d1b33791fa7c9fa15905
unknown <ibmjavanh13@iiht.tech>
2021-04-01 14:46:47 +0530

    Added Employee

--- employee-service/src/main/java/com/ibm/employee/entity/Employee.java
+++ employee-service/src/main/java/com/ibm/employee/entity/Employee.java
@@ -1,5 +1,46 @@
 package com.ibm.employee.entity;
 
-public class Employee {
+import javax.validation.constraints.NotNull;
 
+public class Employee {
+	private String id;
+	@NotNull
+	private String name;
+	@NotNull
+	private String emailId;
+	@NotNull
+	private int mobileNumber;
+	@NotNull
+	private String projectId;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+	public int getMobileNumber() {
+		return mobileNumber;
+	}
+	public void setMobileNumber(int mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+	public String getProjectId() {
+		return projectId;
+	}
+	public void setProjectId(String projectId) {
+		this.projectId = projectId;
+	}
+	
 }
