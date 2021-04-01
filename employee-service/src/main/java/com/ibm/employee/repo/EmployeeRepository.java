b5417915b95f4d9fbc78782c13adcfe69a3db8de
vaiibm <vaiibm17@gmail.com>
2021-04-01 15:12:59 +0530

    added properties

--- employee-service/src/main/java/com/ibm/employee/repo/EmployeeRepository.java
+++ employee-service/src/main/java/com/ibm/employee/repo/EmployeeRepository.java
@@ -2,7 +2,7 @@ package com.ibm.employee.repo;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import com.ibm.entity.Employee;
+import com.ibm.employee.entity.Employee;
 
 public interface EmployeeRepository extends MongoRepository<Employee, String> {
 
