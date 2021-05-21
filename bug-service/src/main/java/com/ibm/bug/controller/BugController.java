c40e1a84d4dfebc7912d9635345dfedcd45b0b3a
Vaibhav Ghadage <vaibhav.ghadage@ibm.com>
2021-05-21 11:44:27 +0530

    added support for crossorigin

--- bug-service/src/main/java/com/ibm/bug/controller/BugController.java
+++ bug-service/src/main/java/com/ibm/bug/controller/BugController.java
@@ -10,6 +10,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -40,6 +41,7 @@ public class BugController {
 	 * 
 	 *                      returns bugId
 	 */
+	@CrossOrigin
 	@PostMapping("/bug")
 	@ResponseStatus(code = HttpStatus.CREATED)
 	String createBug(@RequestBody @Valid Bug bug, BindingResult bindingResult) {
@@ -59,6 +61,7 @@ public class BugController {
 	 * 
 	 * returns list of bugs
 	 */
+	@CrossOrigin
 	@GetMapping("/bug/")
 	List<Bug> getBugs() {
 		return bugService.getBugs();
@@ -71,27 +74,32 @@ public class BugController {
 	 * 
 	 *              returns zero or matching bug
 	 */
-
+	
+	@CrossOrigin
 	@GetMapping("/bug/{name}")
 	List<Bug> getBug(@PathVariable("name") String bugName) {
 		return bugService.getBug(bugName);
 	}
 
+	@CrossOrigin
 	@GetMapping("/bug/status/{name}")
 	List<Bug> getBug(@PathVariable("name") STATUS bugStatus) {
 		return bugService.getBugByStatus(bugStatus);
 	}
 
+	@CrossOrigin
 	@GetMapping("/bug/id/{id}")
 	Optional<Bug> getBugById(@PathVariable("id") String bugId) {
 		return bugService.getBugById(bugId);
 	}
 
+	@CrossOrigin
 	@RequestMapping("/bug/search")
 	List<Bug> findByStatusAndName(@RequestParam("status") STATUS bugStatus, @RequestParam("name") String name) {
 		return bugService.findByStatusAndName(bugStatus, name);
 	}
 	
+	@CrossOrigin
 	@GetMapping("/bug/partialsearch/{name}")
 	List<Bug> getBugByPartialName(@PathVariable("name") String bugName) {
 		return bugService.getBugByPartialName(bugName);
@@ -104,6 +112,8 @@ public class BugController {
 	 * @param bindingResult
 	 * @param bugId
 	 */
+	
+	@CrossOrigin
 	@PutMapping("/bug/{id}")
 	void updateBug(@RequestBody @Valid Bug bug, BindingResult bindingResult, @PathVariable("id") String bugId) {
 		validateBug(bindingResult);
@@ -111,6 +121,8 @@ public class BugController {
 		bug.setId(bugId);
 		bugService.updateBugNew(bug);
 	}
+	
+	@CrossOrigin
 	@DeleteMapping("/bug/{id}")
 	void deleteBug(@PathVariable("id") String bugId) {
 		 bugService.deleteBug(bugId);
